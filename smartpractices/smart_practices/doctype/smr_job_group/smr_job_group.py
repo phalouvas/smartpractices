637572f28f3db2e51be7e61a2534788c490889b0
Panayiotis Halouvas <phalouvas@gmail.com>
2024-03-20 06:00:26 +0000

    added job groups doctype

---
+++ smartpractices/smart_practices/doctype/smr_job_group/smr_job_group.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, KAINOTOMO PH LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class SmrJobGroup(NestedSet):
+	pass

c19d6e63d56c92d3d563937e094138e360c21edb
Panayiotis Halouvas <phalouvas@gmail.com>
2024-03-20 07:00:21 +0000

    Add doctype_js for Customer and create_jobs function

--- smartpractices/smart_practices/doctype/smr_job/smr_job.py
+++ smartpractices/smart_practices/doctype/smr_job/smr_job.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2024, KAINOTOMO PH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.cache_manager import get_doctype_map
 from frappe.model.document import Document
 
-
 class SmrJob(Document):
+	pass
+
+@frappe.whitelist()
+def create_jobs(customer):
+	job_groups = get_doctype_map(
+		"Smr Job Group",
+		"Smr Job Non Groups",
+		filters={"is_group": 0}
+	)
+	existing_jobs = get_doctype_map(
+		"Smr Job",
+		"Smr Job Non Groups",
+		filters={"customer": customer}
+	)
 	pass

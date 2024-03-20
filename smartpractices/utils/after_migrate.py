20a807299a0f81bc802b95405ee9f9104849a74c
Panayiotis Halouvas <phalouvas@gmail.com>
2024-03-20 08:47:54 +0000

    Add after_migrate hook to give read access to Sales User and Sales Manager

--- smartpractices/utils/after_migrate.py
+++ smartpractices/utils/after_migrate.py
@@ -1,8 +1,7 @@
 import frappe
 from frappe.core.doctype.data_import.data_import import import_file
 import os
-import frappe
-from frappe.core.doctype.data_import.data_import import import_file
+from frappe.permissions import add_permission, update_permission_property
 
 def import_job_groups():
     # Check if Smr Job Group already has data
@@ -17,3 +16,28 @@ def import_job_groups():
 
     file_path = os.path.join(file_dir, 'Smr Job Group 2.csv')
     import_file("Smr Job Group", file_path, "Insert", False, True)
+
+def give_read_access():
+    add_permission("Smr Job Group", "Sales User", 0)
+    update_permission_property("Smr Job Group", "Sales User", 0, "read", 1)
+    add_permission("Smr Job Group", "Sales Manager", 0)
+    update_permission_property("Smr Job Group", "Sales Manager", 0, "read", 1)
+    update_permission_property("Smr Job Group", "Sales Manager", 0, "write", 1)
+    update_permission_property("Smr Job Group", "Sales Manager", 0, "create", 1)
+    update_permission_property("Smr Job Group", "Sales Manager", 0, "delete", 1)
+    update_permission_property("Smr Job Group", "Sales Manager", 0, "share", 1)
+    update_permission_property("Smr Job Group", "Sales Manager", 0, "report", 1)
+    update_permission_property("Smr Job Group", "Sales Manager", 0, "print", 1)
+    update_permission_property("Smr Job Group", "Sales Manager", 0, "email", 1)
+
+    add_permission("Smr Job", "Sales User", 0)
+    update_permission_property("Smr Job", "Sales User", 0, "read", 1)
+    add_permission("Smr Job", "Sales Manager", 0)
+    update_permission_property("Smr Job", "Sales Manager", 0, "read", 1)
+    update_permission_property("Smr Job", "Sales Manager", 0, "write", 1)
+    update_permission_property("Smr Job", "Sales Manager", 0, "create", 1)
+    update_permission_property("Smr Job", "Sales Manager", 0, "delete", 1)
+    update_permission_property("Smr Job", "Sales Manager", 0, "share", 1)
+    update_permission_property("Smr Job", "Sales Manager", 0, "report", 1)
+    update_permission_property("Smr Job", "Sales Manager", 0, "print", 1)
+    update_permission_property("Smr Job", "Sales Manager", 0, "email", 1)

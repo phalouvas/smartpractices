eb9f5cbde2df82aa4be736bce43b154a6dd9dffc
Panayiotis Halouvas <phalouvas@gmail.com>
2024-03-20 08:29:51 +0000

    Add in_list_view and in_standard_filter properties to parent_smr_job_group field. Update modified timestamp in Smr Job Group document. Add check for existing data in import_job_groups function.

--- smartpractices/utils/after_migrate.py
+++ smartpractices/utils/after_migrate.py
@@ -1,5 +1,3 @@
-# smartpractices/utils.py
-
 import frappe
 from frappe.core.doctype.data_import.data_import import import_file
 import os
@@ -7,6 +5,9 @@ import frappe
 from frappe.core.doctype.data_import.data_import import import_file
 
 def import_job_groups():
+    # Check if Smr Job Group already has data
+    if frappe.get_all("Smr Job Group"):
+        return
 
     # Get the directory path of the current file
     file_dir = os.path.dirname(os.path.abspath(__file__))

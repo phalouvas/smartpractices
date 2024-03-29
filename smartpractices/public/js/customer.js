024e4e8b2f938e74195332e3db970aed652c135a
Panayiotis Halouvas <phalouvas@gmail.com>
2024-03-29 06:23:38 +0000

    Refactor customer.js button code

--- smartpractices/public/js/customer.js
+++ smartpractices/public/js/customer.js
@@ -1,25 +1,25 @@
 frappe.ui.form.on('Customer', {
-    refresh: function (frm) {
-        if (!frm.is_new()) {
-            frm.add_custom_button(__('Create Jobs'), function () {
-                frappe.confirm(__("Are you sure you want to proceed?"), () => {
-                    frappe.call({
-                      method: "smartpractices.smart_practices.doctype.smr_job.smr_job.create_jobs",
-                      args: {
-						customer: frm.doc.name,
-					},
-                      freeze: true,
-                      freeze_message: __("Creating Jobs Data..."),
-                      callback: function(r) {
-                        //frappe.ui.toolbar.clear_cache();
-                        frappe.show_alert({
-                          message: __("Jobs created"),
-                          indicator: "green"
-                        });
-                      }
-                    });
-                  });
-            }).addClass('btn-primary');
-        }
-    },
+  refresh: function (frm) {
+    if (!frm.is_new()) {
+      frm.add_custom_button(__('Create Jobs'), function () {
+        frappe.confirm(__("Are you sure you want to proceed?"), () => {
+          frappe.call({
+            method: "smartpractices.smart_practices.doctype.smr_job.smr_job.create_jobs",
+            args: {
+              customer: frm.doc.name,
+            },
+            freeze: true,
+            freeze_message: __("Creating Jobs Data..."),
+            callback: function (r) {
+              //frappe.ui.toolbar.clear_cache();
+              frappe.show_alert({
+                message: __("Jobs created"),
+                indicator: "green"
+              });
+            }
+          });
+        });
+      }).addClass('btn-primary');
+    }
+  },
 });

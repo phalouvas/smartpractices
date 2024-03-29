bc67e31cfcaddfda22ec7e4718d5aab1ccebf821
Panayiotis Halouvas <phalouvas@gmail.com>
2024-03-29 06:48:42 +0000

    Add custom buttons for creating Quotation and Sales Order

--- smartpractices/public/js/customer.js
+++ smartpractices/public/js/customer.js
@@ -1,7 +1,8 @@
 frappe.ui.form.on('Customer', {
   refresh: function (frm) {
     if (!frm.is_new()) {
-      frm.add_custom_button(__('Create Quotation'), function () {
+
+      frm.add_custom_button(__('Quotation'), function () {
         frappe.confirm(__("Are you sure you want to proceed?"), () => {
           frappe.call({
             method: "smartpractices.utils.customer.create_quotation",
@@ -21,7 +22,30 @@ frappe.ui.form.on('Customer', {
             }
           });
         });
-      }, __('Smart Practices'));
+      }, __('Create'));
+
+      frm.add_custom_button(__('Sales Order'), function () {
+        frappe.confirm(__("Are you sure you want to proceed?"), () => {
+          frappe.call({
+            method: "smartpractices.utils.customer.create_sales_order",
+            args: {
+              customer: frm.doc.name,
+            },
+            freeze: true,
+            freeze_message: __("Creating Sales Order..."),
+            callback: function (r) {
+              //frappe.ui.toolbar.clear_cache();
+              frappe.show_alert({
+                message: __("Sales Order created"),
+                indicator: "green"
+              });
+              frappe.ui.toolbar.clear_cache();
+              frappe.set_route("Form", "Sales Order", r.message);
+            }
+          });
+        });
+      }, __('Create'));
+
     }
   },
 });
--- smartpractices/utils/customer.py
+++ smartpractices/utils/customer.py
@@ -25,3 +25,21 @@ def create_quotation(customer):
     quotation.insert()
     quotation.submit()
     return quotation.name
+
+@frappe.whitelist()
+def create_sales_order(customer):
+
+    items = get_items()
+    sales_order = frappe.new_doc("Sales Order")
+    sales_order.customer = customer
+    # set delivery_date as 7 days from today
+    sales_order.delivery_date = frappe.utils.add_days(frappe.utils.nowdate(), 7)
+    sales_order.items = []
+    for item in items:
+        sales_order.append("items", {
+            "item_code": item.name,
+            "qty": 1
+        })
+    sales_order.insert()
+    sales_order.submit()
+    return sales_order.name

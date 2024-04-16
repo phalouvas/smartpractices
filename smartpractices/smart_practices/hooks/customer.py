8856b6cb20f35f4ec0259a22e3352240cc1fc062
Panayiotis Halouvas <phalouvas@gmail.com>
2024-04-16 06:46:39 +0000

    Refactor customer.py to delete related_person documents when custom_customer_information is deleted

--- smartpractices/smart_practices/hooks/customer.py
+++ smartpractices/smart_practices/hooks/customer.py
@@ -14,5 +14,9 @@ def after_insert(doc, method):
 def after_delete(doc, method):
     # if custom_customer_information is not empty
     if doc.get('custom_customer_information'):
-        # delete the customer consent document
+        # delete the related_person document where the customer is the custom_customer_information
+        related_person = frappe.get_all("Related Person", filters={'customer': doc.get('custom_customer_information')}) or []
+        for person in related_person:
+            frappe.delete_doc("Related Person", person.get('name'))
+        # delete the custom_customer_information document
         frappe.delete_doc("Customer Information", doc.get('custom_customer_information'))

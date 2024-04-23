efc054c95f1b09447c3b937c269dd899c4ebe649
Panayiotis Halouvas <phalouvas@gmail.com>
2024-04-23 04:45:56 +0000

    Fix link_doctype and link_fieldname in after_migrate.py

--- smartpractices/utils/after_migrate.py
+++ smartpractices/utils/after_migrate.py
@@ -2,14 +2,14 @@ import frappe
 
 def customer_links():
 	# Check "DocType Link" if there is entry for link_doctype with value "Register Company"
-	if not frappe.db.exists("DocType Link", {"link_doctype": "Register Company", "parent": "Company"}):
+	if not frappe.db.exists("DocType Link", {"link_doctype": "Register Company", "parent": "Customer"}):
 		# Create a new entry in "DocType Link" with link_doctype "Register Company"
 		frappe.get_doc({
 			"doctype": "DocType Link",
 			"link_doctype": "Register Company",
-			"link_fieldname": "company",
+			"link_fieldname": "customer",  # Assuming "Register Company" DocType has a link field to "Customer"
 			"parenttype": "Customize Form",
-			"parent": "Company",
+			"parent": "Customer",
 			"parentfield": "links",
 			"group": "Orders",
 			"custom": 1

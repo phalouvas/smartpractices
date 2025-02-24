b9be7fcb5dc34b2400fc6d0b2f1c0be3600134eb
Panayiotis Halouvas <phalouvas@gmail.com>
2025-02-24 04:42:44 +0000

    feat: Enhance address and contact management in Related Person and Company Person documents

--- smartpractices/smart_practices/doctype/related_person/related_person.js
+++ smartpractices/smart_practices/doctype/related_person/related_person.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2024, KAINOTOMO PH LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Related Person", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Related Person", {
+    refresh(frm) {
+        if (frm.doc.__islocal) {
+			hide_field(["address_html", "contact_html", "address_contacts"]);
+			frappe.contacts.clear_address_and_contact(frm);
+		} else {
+			unhide_field(["address_html", "contact_html", "address_contacts"]);
+			frappe.contacts.render_address_and_contact(frm);
+		}
+    },
+});
--- smartpractices/smart_practices/doctype/related_person/related_person.py
+++ smartpractices/smart_practices/doctype/related_person/related_person.py
@@ -3,7 +3,21 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe.defaults
+from frappe import _, msgprint, qb
+from frappe.contacts.address_and_contact import (
+	delete_contact_and_address,
+	load_address_and_contact,
+)
+from frappe.model.mapper import get_mapped_doc
+from frappe.model.naming import set_name_by_naming_series, set_name_from_naming_options
+from frappe.model.utils.rename_doc import update_linked_doctypes
+from frappe.utils import cint, cstr, flt, get_formatted_email, today
+from frappe.utils.deprecations import deprecated
+from frappe.utils.user import get_users_with_role
 
 class RelatedPerson(Document):
-	pass
+	
+	def onload(self):
+		"""Load address and contacts in `__onload`"""
+		load_address_and_contact(self)
